b776993acac0fcc6695569d88c0b77c66c0a4960
Renars Mednieks <rena0968@easv365.dk>
2024-02-04 14:58:07 +0100

    .

--- sheriff.config.ts
+++ sheriff.config.ts
@@ -2,29 +2,33 @@ import {noDependencies, sameTag, SheriffConfig} from '@softarc/sheriff-core';
 
 export const sheriffConfig: SheriffConfig = {
 
-  version: 1,
-  excludeRoot: false,
-  depRules: {
-    root: ['*'],
-    'domain:*': [sameTag, 'domain:shared'],
-    'type:feature': [
-      'type:ui',
-      'type:data',
-      'type:util',
-    ],
-    'type:ui': ['type:data', 'type:util'],
-    'type:data': ['type:util'],
-    'type:util': noDependencies,
-  },
-  tagging: {
-    // 'src/app': {
-    //   'domains/<domain>': {
-    //     'feature/': ['domain:<domain>', 'type:feature'],
-    //     'data/': ['domain:<domain>', 'type:data'],
-    //     'util/': ['domain:<domain>', 'type: util'],
-    //     'ui/': ['domain:<domain>', 'type: ui']
-    //   }
-    // },
-    'src/app/domains/<domain>/<type>': ['domain:<domain>', 'type:<type>'],
-  },
+    version: 1,
+    excludeRoot: false,
+    depRules: {
+        root: ['*'],
+        'domain:*': [sameTag, 'domain:shared'],
+        'type:feature': [
+            'type:ui',
+            'type:data',
+            'type:util',
+        ],
+        'type:ui': ['type:data', 'type:util'],
+        'type:data': ['type:util'],
+        'type:util': noDependencies,
+    },
+    tagging: {
+        // 'src/app': {
+        //   'domains/<domain>': {
+        //     'feature/': ['domain:<domain>', 'type:feature'],
+        //     'data/': ['domain:<domain>', 'type:data'],
+        //     'util/': ['domain:<domain>', 'type: util'],
+        //     'ui/': ['domain:<domain>', 'type: ui']
+        //   }
+        // },
+        // 'src/app/domains/<domain>/<type>': ['domain:<domain>', 'type:<type>'],
+        'src/app/domains/home/<type>': ['domain:home', 'type:<type>'],
+        'src/app/domains/profile/<type>': ['domain:profile', 'type:<type>'],
+        'src/app/domains/shared/<type>': ['domain:shared', 'type:<type>'],
+        'src/app/domains/sign-in/<type>': ['domain:sign-in', 'type:<type>']
+    },
 };
